e089890a25e53b389c0e585e3641586f1707d41d
chuhezhe0807 <chuhezhe0807@163.com>
2026-02-13 14:51:23 +0800

    chore: MetaObjectHandlerConfig 集成到 MybatisPlusConfig 中;

--- rag-common-service/src/main/java/com/chuhezhe/ragcommonservice/config/MybatisPlusConfig.java
+++ rag-common-service/src/main/java/com/chuhezhe/ragcommonservice/config/MybatisPlusConfig.java
@@ -1,13 +1,17 @@
 package com.chuhezhe.ragcommonservice.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDateTime;
+
 @Configuration
-public class MybatisPlusConfig {
+public class MybatisPlusConfig implements MetaObjectHandler {
 
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
@@ -15,5 +19,24 @@ public class MybatisPlusConfig {
         // 添加分页插件（自动识别数据库类型，默认支持 MySQL）
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
+    }
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        if (metaObject.hasSetter("createdAt")) {
+            this.strictInsertFill(metaObject, "createdAt", LocalDateTime.class, LocalDateTime.now());
+        }
+
+        if (metaObject.hasSetter("updatedAt")) {
+            this.strictInsertFill(metaObject, "updatedAt", LocalDateTime.class, LocalDateTime.now());
+        }
+    }
+
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        if (metaObject.hasSetter("updatedAt")) {
+            this.strictUpdateFill(metaObject, "updatedAt", LocalDateTime.class, LocalDateTime.now());
+        }
     }
 }
